5db57b2dedd29056b147befa7e3acb43bc57d841
Superb-Man <kazitoriqe207@gmail.com>
2024-10-15 02:52:05 +0600

    dropout layer added

--- FNN/classes.py
+++ FNN/classes.py
@@ -1,6 +1,17 @@
 import numpy as np
-# import logistic_regression from sklearn
-from sklearn.linear_model import LogisticRegression
+
+
+# batchsize = number of samples for now
+# num_units = next layer neurons/num_of_features
+# x = input data
+# w = weights
+# grad_output = gradient of loss with respect to the output of this layer
+class Layer:
+    def forward(self, input):
+        raise NotImplementedError
+
+    def backward(self, grad_output):
+        raise NotImplementedError
 
 class Loss:
     def forward(self, y_true, y_pred):
@@ -20,6 +31,21 @@ class Activation:
     
     def backward(self, input, grad_output):
         raise NotImplementedError
+        
+
+class Dropout(Layer):
+    def __init__(self, p=0.2):
+        self.p = 1 - p
+        self.mask = None
+    
+    def forward(self, input):
+        np.random.seed(0)
+        self.mask = np.random.binomial(1, self.p, size=input.shape) / self.p
+        return input * self.mask
+    
+    def backward(self, grad_output):
+        return grad_output * self.mask , None , None
+
     
 class ReLU(Activation):
     def forward(self,input):
@@ -29,6 +55,7 @@ class ReLU(Activation):
         self.input = input
         return np.maximum(0,input)
     
+    # grad_input = dx 
     def backward(self,grad_output):
         "'grad_output: gradient of loss with respect to the output of this layer (batch_size, num_channels, width, height)'"
         "'grad_input: gradient of loss with respect to the input of this layer (batch_size, num_channels, width, height)'"
@@ -48,27 +75,23 @@ class Sigmoid(Activation):
         "'grad_output: gradient of loss with respect to the output of this layer (batch_size, num_channels, width, height)'"
         "'grad_input: gradient of loss with respect to the input of this layer (batch_size, num_channels, width, height)'"
         # back-propagate through the sigmoid function
-        # derivative of sigmoid function is sigmoid(x) * (1 - sigmoid(x))
+        # f'(x) = grad_output * f(x) * (1 - f(x))
         return grad_output * self.output * (1 - self.output)
 
 
 
-class Layer:
-    # batchsize = number of samples for now
-    # num_units = next layer neurons/num_of_features
-    # x = input data
-    # w = weights
-    # grad_output = gradient of loss with respect to the output of this layer
-    def forward(self, input):
-        # z = x * w.T => (batch_size, num_features) * (num_features, num_units) = (batch_size, num_units)
-        raise NotImplementedError
-    
-    def backward(self, grad_output):
-        # del = dL/da * da/dz * dz/dw  
-        # dw = x.T * grad_output => (num_features, batch_size) * (batch_size, num_units) = (num_features, num_units)
-        # dx = grad_output * w => (batch_size, num_units) * (num_units, num_features) = (batch_size, num_features)
-        raise NotImplementedError
-    
+
+# formula for dense layer
+# forward propagation
+# self.output = activation(self.input * self.weights)
+
+# backpropagation
+# grad_output (from next layer) ================> dL/dz = dL/da * da/dz    
+
+# grad_weights = input.T * grad_output =========> dL/dweights = x.T * dL/dz
+# grad_bias = sum(grad_output)  ================> dL/db = sum(dL/dz)
+# grad_input = grad_output * weights.T =========> dL/dx = dL/dz * w.T
+
 class Dense(Layer):
     '"Dense layer is a fully connected layer"'
     "'input_size: number of input features"
@@ -78,8 +101,10 @@ class Dense(Layer):
         if initializer == 'xaiver':
             denominator = np.sqrt(input_size)
         
-        # add 1 for bias term
-        self.weights = np.random.randn(input_size+1,output_size) / denominator
+        np.random.seed(0)
+        
+        self.weights = np.random.randn(input_size,output_size) / denominator
+        self.bias = np.zeros(output_size)
         self.activation = activation
 
     def forward(self,input):
@@ -87,30 +112,30 @@ class Dense(Layer):
         "'output: output data with shape (batch_size, output_size)'"
         # covert input to numpy array
         input = np.array(input) if not isinstance(input,np.ndarray) else input
-        # check if input has bias term
-        if input.shape[1] == self.weights.shape[0] -1:
-            # add an extra column of ones for bias term
-            input = np.insert(input,0,1,axis = 1)
 
-        self.input = input
-        # print(self.weights.shape,self.input.shape)
-
-        self.output = np.dot(self.input,self.weights)
+        self.input = input 
+        self.output = np.dot(self.input,self.weights) + self.bias # z = x * weights + bias
 
         if self.activation is not None:
-            self.output = self.activation.forward(self.output)
+            self.output = self.activation.forward(self.output) # a = activator(z)
         return self.output
     
     def backward(self,grad_output):
         "'grad_output: gradient of loss with respect to the output of this layer (batch_size, output_size)'"
         "'grad_input: gradient of loss with respect to the input of this layer (batch_size, input_size)'"
-        grad_output = self.activation.backward(grad_output) if self.activation is not None else grad_output
-        grad_weights = np.dot(self.input.T,grad_output)
+        grad_output = self.activation.backward(grad_output) if self.activation is not None else grad_output # dL/da * da/dz
+        grad_weights = np.dot(self.input.T,grad_output) # dL/dweights = x.T * dL/dz
+        grad_bias = np.sum(grad_output,axis = 0)        # dL/db = sum(dL/dz)
 
-        # donot backpropagate through bias term
-        grad_input = np.dot(grad_output,self.weights[:-1].T)
+        grad_clip = 1.
+        grad_weights = np.clip(grad_weights,-grad_clip,grad_clip)
+        grad_bias = np.clip(grad_bias,-grad_clip,grad_clip)
 
-        return grad_input , grad_weights
+        grad_input = np.dot(grad_output,self.weights.T) # dL/dx = dL/dz * w.T
+
+        dL = [grad_input,grad_weights,grad_bias] 
+
+        return dL
 
 
 class Flatten(Layer):
@@ -124,7 +149,7 @@ class Flatten(Layer):
     def backward(self,grad_output):
         "'grad_output: gradient of loss with respect to the output of this layer (batch_size, num_channels*width*height)'"
         "'grad_input: gradient of loss with respect to the input of this layer (batch_size, num_channels, width, height)'"
-        return grad_output.reshape(self.input_shape) , None
+        return grad_output.reshape(self.input_shape) , None , None
     
 
 class CrossEntropyLoss(Loss):
@@ -155,10 +180,13 @@ class CrossEntropyLoss(Loss):
         m = y_pred.shape[0] # number of samples
 
         # calculate gradient of loss with respect to the output of this layer
-        grad_output = (-y_true / y_pred) / m
+        grad_output = -y_true / y_pred / m
         return grad_output
         
     
+# Stochastic Gradient Descent formula
+# weights = weights - lr * dL/dweights
+# bias = bias - lr * dL/dbias
 class SGD(Optimizer):
     def __init__(self,lr = 0.001):
         self.lr = lr
@@ -166,41 +194,58 @@ class SGD(Optimizer):
     # call the backward method of each layer to update the weights
     def step(self,layers,grad_output):
         for layer in reversed(layers):
-            grad_output,grad_weights = layer.backward(grad_output)
+            grad_output,grad_weights ,grad_bias = layer.backward(grad_output)
             if grad_weights is not None:
                 layer.weights -= self.lr * grad_weights
+                layer.bias -= self.lr * grad_bias
         
         return grad_output
 
-
+# Adam Optimizer formulas
+# m = beta1 * m + (1 - beta1) * dL/dweights
+# v = beta2 * v + (1 - beta2) * (dL/dweights) ** 2
+# m_hat = m / (1 - beta1 ** t)
+# v_hat = v / (1 - beta2 ** t)
+# weights = weights - lr * m_hat / (np.sqrt(v_hat) + epsilon)
 class Adam(Optimizer):
     def __init__(self, lr=0.001, beta1=0.9, beta2=0.999, epsilon=1e-8):
         self.lr = lr
         self.beta1 = beta1
         self.beta2 = beta2
         self.epsilon = epsilon
-        self.m = {}  # momentum for each layer
-        self.v = {}  # variance for each layer
+        self.m = {}
+        self.v = {}
         self.t = 0
         
     def step(self, layers, grad_output):
         self.t += 1
         
-        for idx, layer in enumerate(reversed(layers)):     
-            grad_output, grad_weights = layer.backward(grad_output)       
+        for idx, layer in enumerate(reversed(layers)):
+
+            grad_output, grad_weights, grad_bias = layer.backward(grad_output) 
+
             if grad_weights is not None:
-                # print(idx)
+
                 if idx not in self.m:
-                    self.m[idx] = np.zeros_like(grad_weights)
-                    self.v[idx] = np.zeros_like(grad_weights)
-                self.m[idx] = self.beta1 * self.m[idx] + (1 - self.beta1) * grad_weights
-                self.v[idx] = self.beta2 * self.v[idx] + (1 - self.beta2) * (grad_weights ** 2)
+                    self.m[idx] = {'w': np.zeros_like(grad_weights), 'b': np.zeros_like(grad_bias)}
+                    self.v[idx] = {'w': np.zeros_like(grad_weights), 'b': np.zeros_like(grad_bias)}
                 
 
-                m_hat = self.m[idx] / (1 - self.beta1 ** self.t)
-                v_hat = self.v[idx] / (1 - self.beta2 ** self.t)
 
-                layer.weights -= self.lr * m_hat / (np.sqrt(v_hat) + self.epsilon)
+                self.m[idx]['w'] = self.beta1 * self.m[idx]['w'] + (1 - self.beta1) * grad_weights
+                self.v[idx]['w'] = self.beta2 * self.v[idx]['w'] + (1 - self.beta2) * grad_weights ** 2
+
+                self.m[idx]['b'] = self.beta1 * self.m[idx]['b'] + (1 - self.beta1) * grad_bias
+                self.v[idx]['b'] = self.beta2 * self.v[idx]['b'] + (1 - self.beta2) * grad_bias ** 2
+
+                m_hat_w = self.m[idx]['w'] / (1 - self.beta1 ** self.t)
+                v_hat_w = self.v[idx]['w'] / (1 - self.beta2 ** self.t)
+
+                m_hat_b = self.m[idx]['b'] / (1 - self.beta1 ** self.t)
+                v_hat_b = self.v[idx]['b'] / (1 - self.beta2 ** self.t)
+
+                layer.weights = layer.weights - self.lr * m_hat_w / (np.sqrt(v_hat_w) + self.epsilon)
+                layer.bias = layer.bias - self.lr * m_hat_b / (np.sqrt(v_hat_b) + self.epsilon)
 
         return grad_output
 
@@ -215,8 +260,11 @@ class FNN:
     def add(self,layer):
         self.layers.append(layer)
     
-    def forward(self,input):
+    def forward(self,input,training = True):
         for layer in self.layers:
+            if training == False:
+                if isinstance(layer,Dropout):
+                    continue
             input = layer.forward(input)
         return input
     
@@ -224,7 +272,7 @@ class FNN:
         return optimizer.step(self.layers,grad_output)
 
     def train(self,x,y_true,epochs):
-        lrs = [0.005,0.0025,0.001,0.0005,0.0001]
+        lrs = [0.001]
         for lr in lrs:
             self.optimizer.lr = lr
             for epoch in range(epochs):
@@ -235,13 +283,14 @@ class FNN:
                 x = self.backward(x)
 
     def predict(self,x):
-        return self.forward(x)
+
+        return self.forward(x,training = False)
 
 
 
 if __name__ == '__main__':
     sample_size = 1000
-    num_classes = 4
+    num_classes = 2
     input_shape = (sample_size, 3, 32, 32)
     
     # generatae same random data each time
@@ -250,21 +299,22 @@ if __name__ == '__main__':
     # normalize the data
     x = (x - np.mean(x)) / np.std(x)
 
+    np.random.seed(0)
     y_true = np.eye(num_classes)[np.random.choice(num_classes, sample_size)]  # random one-hot labels
     print(y_true.shape)
 
-    x_train = x[:800]
-    y_train = y_true[:800]
+    x_train = x[:1000]
+    y_train = y_true[:1000]
 
-    x_test = x[800:]
-    y_test = y_true[800:]
+    x_test = x[1000:]
+    y_test = y_true[1000:]
 
     
     # Define the model layers
     flatten = Flatten()
     dense1 = Dense(3*32*32, 100, activation=ReLU())
-    dense2 = Dense(100, 50, activation=ReLU())
-    dense3 = Dense(50, 4, activation=Sigmoid())
+    dense2 = Dense(100, 100, activation=ReLU())
+    dense3 = Dense(100, num_classes, activation=Sigmoid())
 
     # Define loss function and optimizer
     loss_function = CrossEntropyLoss()
@@ -274,12 +324,14 @@ if __name__ == '__main__':
     model = FNN(optimizer=optimizer, loss=loss_function)
     model.add(flatten)
     model.add(dense1)
+    model.add(Dropout())
     model.add(dense2)
+    model.add(Dropout())
     model.add(dense3)
 
-    model.train(x_train, y_train, epochs=10)
-    y_pred = model.predict(x_test)
+    model.train(x_train, y_train, epochs=100)
+    y_pred = model.predict(x_train)
     print(y_pred)
 
-    accuracy = np.mean(np.argmax(y_pred, axis=1) == np.argmax(y_test, axis=1))
+    accuracy = np.mean(np.argmax(y_pred, axis=1) == np.argmax(y_train, axis=1))
     print(f"Accuracy: {accuracy}")
